516f7318e177e4ba71b03f2a654c1506214449ad
jposorio12 <juan.osoriom@ab-inbev.com>
2022-08-23 16:53:37 -0500

    a

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
-  return <div></div>;
-}
+import useRoutes from "./Routes";
+// import { store, persistor } from "./Redux/Store/Store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+
+const App = () => {
+  // const { Router } = useRoutes();
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Router store={store} />
+      </PersistGate>
+    </Provider>
+  );
+};
 
 export default App;
